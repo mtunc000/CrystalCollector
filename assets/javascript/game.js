9b8c9b3c82b7cf706a729f2d044b24fb51f56db0
Mustafa Tunc <mtunc@hotmail.com>
2018-08-21 23:24:41 -0500

    crystal value assigned

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var lost;
 var result;
 var randomGeneratedNumber;
 var crystalValue;
+var start = 0;
 
 // Random number generator between 19 and 120
 
@@ -18,16 +19,32 @@ randomGeneratedNumber = Math.floor(Math.random()*101)+19;
 
 for (i=0; i<4; i++){
     var crystal = $("<div>");
-    crystal.attr("class",'crystal')
-    console.log (crystal);
 
-    // random crystal value generator
-
+     // random crystal value generator
     crystalValue = Math.floor(Math.random()*11)+1;
+    crystal.attr({
+        "class":'crystal',
+        "data_random":crystalValue});
+
+    console.log (crystal);
+   
     $(".crystals").append(crystal);
     crystal.html(crystalValue);
     console.log(crystalValue);
 }
+
+// numbers collected from crystals and added towards the result 
+//on click
+
+    $(document).on('click',".crystal",function(){
+        console.log($(this).attr("data_random"));
+      
+       result = parseInt($(this).attr("data_random"));
+        
+         start += result;
+        $(".start").html('Start:' +start);
+
+    })
 
 
 
