13a9c0dca90441b53e8a0f1dc744957917b97b3a
Mustafa Tunc <mtunc@hotmail.com>
2018-08-25 05:44:11 -0500

    collector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,72 @@
 // Crystal Collector Game
 
-var wins;
-var lost;
-var result;
+var wins = 0;
+var lost = 0;
+//var result;
 var randomGeneratedNumber;
 var crystalValue;
 var start = 0;
 
 // Random number generator between 19 and 120
+var resetAndStart= function(){ 
+    $(".crystals").empty();
 
+    var images= [
+        '',
+        '',
+        '',
+        '']
 randomGeneratedNumber = Math.floor(Math.random()*101)+19;
     $(".result").html('randomGeneratedNumber:' + randomGeneratedNumber);
-
-    console.log(randomGeneratedNumber);
-
 
 // random crystal selector
 
 for (i=0; i<4; i++){
-    var crystal = $("<div>");
-
-     // random crystal value generator
+ // random crystal value generator
     crystalValue = Math.floor(Math.random()*11)+1;
+
+    var crystal = $("<div>");
     crystal.attr({
         "class":'crystal',
-        "data_random":crystalValue});
+        "data-random":crystalValue});
 
-    console.log (crystal);
    
-    $(".crystals").append(crystal);
     crystal.html(crystalValue);
-    console.log(crystalValue);
+    $(".crystals").append(crystal);
+    
+    
 }
-
+}
+resetAndStart();
 // numbers collected from crystals and added towards the result 
 //on click
 
     $(document).on('click',".crystal",function(){
-        console.log($(this).attr("data_random"));
+        console.log($(this).attr("data-random"));
       
-       result = parseInt($(this).attr("data_random"));
+       let result = parseInt($(this).attr('data-random'));
         
          start += result;
-        $(".start").html('Start:' +start);
+        $(".start").html('Start : ' +start);
 
-    })
+        if(start > randomGeneratedNumber) {
+            lost++;
+        $(".lost").html('Lost : ' +lost);
+
+        start =0;
+        resetAndStart();
+        }
+        
+        else if(start === randomGeneratedNumber) {
+            wins++;
+        $(".wins").html('Wins : ' +wins); 
+        start = 0;
+        resetAndStart();
+        }
+        
+    
+
+    });
 
 
 
